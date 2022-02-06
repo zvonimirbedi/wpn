341e6c4489e9346db9cc3f0e2a2e44d0d0b2a46e
zvonimirbedi <zvonimirbedi@gmail.com>
2022-02-06 17:52:38 +0100

    feature/added_bump_memory_docker

--- src/main/java/com/wpn/configuration/ConfigurationSecurity.java
+++ src/main/java/com/wpn/configuration/ConfigurationSecurity.java
@@ -4,11 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableWebFluxSecurity
 public class ConfigurationSecurity {
